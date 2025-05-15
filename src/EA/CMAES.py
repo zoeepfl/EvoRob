26acb68b86c988587588ec0dcb97aa1ca9ad5f52
Tifaine <tifaine.mezencev@epfl.ch>
2025-05-15 10:47:50 +0200

    safe

--- src/EA/CMAES.py
+++ src/EA/CMAES.py
@@ -39,8 +39,8 @@ class CMAES():
 
     def load_cmaes(self):
         #TODO
-        lower_bounds = ...  # lower bounds per dimension !! check dimensions
-        upper_bounds = ...  # upper bounds per dimension
+        lower_bounds = [self.min] * self.n_params  # lower bounds per dimension !! check dimensions
+        upper_bounds = [self.max] * self.n_params  # upper bounds per dimension
         cmaes_params = {
             'popsize': ...,
             'bounds': (lower_bounds, upper_bounds),
@@ -80,7 +80,7 @@ class CMAES():
 
     def initialise_x0(self, num_parameters):
         #TODO
-        mean_vector = ...
+        mean_vector = np.random.uniform(low=self.min, high=self.max, size=num_parameters)
         return mean_vector
 
     def save_checkpoint(self):
